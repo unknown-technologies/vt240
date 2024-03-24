5f5bef224069d4f1135c512adb00b75c4eac7a81
sateffen <sateffen@gmail.com>
2024-03-24 01:04:00 +0100

    Correct Codestyle

--- web/include/keyboard.h
+++ web/include/keyboard.h
@@ -2,9 +2,7 @@
 #define __KEYBOARD_H__
 
 #include "vt.h"
-#include <emscripten/html5.h>
 
 void VT240InitKeyboard(VT240* vt);
-EM_BOOL handleKeyboardInput(int eventType, const EmscriptenKeyboardEvent *e, void* userData);
 
 #endif
